637aadc3f45cf04d1073b8dc2ec71dcca2370156
Austin N <nazanator@hotmail.com>
2020-05-24 18:34:36 -0600

    Add dynamic classes, and toggle functionality with some css

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -14,4 +14,21 @@ export class TodoItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes
+  }
+
+  onToggle(todo:Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo:Todo) {
+    console.log("delete")
+  }
+
 }
